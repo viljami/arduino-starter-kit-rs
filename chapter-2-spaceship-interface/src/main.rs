02f1a6730c663c1a65f2f645eac0dcdb47aa6236
Viljami Peltola <viljami.peltola@iki.fi>
2023-12-15 14:07:47 +0200

    Improve UX

--- chapter-2-spaceship-interface/src/main.rs
+++ chapter-2-spaceship-interface/src/main.rs
@@ -3,6 +3,35 @@
 
 use panic_halt as _;
 
+type StepsLeft = u8;
+
+struct Count {
+    start: StepsLeft,
+    current: StepsLeft,
+}
+
+impl Count {
+    fn new(start: StepsLeft) -> Self {
+        Count {
+            start,
+            current: start
+        }
+    }
+
+    fn decrement(&self) -> Self {
+        Count {
+            start: self.start,
+            current: self.current - 1,
+        }
+    }
+}
+
+enum State {
+    Loading(Count),
+    Idle,
+    Phasers(Count),
+}
+
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
@@ -15,26 +44,88 @@ fn main() -> ! {
     let mut red_1 = pins.d4.into_output();
     let mut red_2 = pins.d5.into_output();
 
+    let mut current_state = State::Loading(Count::new(120));
+
     loop {
         reassuring_led.toggle();
 
-        if button.is_low() {
-            green.set_high();
-            red_1.set_low();
-            red_2.set_low();
-        } else {
-            green.set_low();
-            red_1.set_low();
-            red_2.set_high();
+        current_state = match current_state {
+            State::Loading(count) => {
+                match count.current {
+                    x if x == count.start => {
+                        red_1.set_low();
+                        red_2.set_low();
+                        green.set_low();
+                    },
 
-            arduino_hal::delay_ms(250);
+                    x if x >= count.start / 4 * 3 => {
+                        red_2.set_high()
+                    },
 
-            red_1.set_high();
-            red_2.set_low();
+                    x if x >= count.start / 4 * 2 => {
+                        red_1.set_high()
+                    }
 
-            arduino_hal::delay_ms(250);
-        }
+                    x if x >= count.start / 4 => {
+                        red_2.set_low()
+                    },
 
-        arduino_hal::delay_ms(500);
+                    x if x < count.start / 4 => {
+                        red_1.set_low()
+                    },
+
+                    _ => (),
+                }
+
+                if count.current == 0 {
+                    State::Idle
+                } else {
+                    State::Loading(count.decrement())
+                }
+            },
+
+            State::Idle => {
+                red_1.set_low();
+                red_2.set_low();
+                green.set_high();
+
+                if button.is_low() {
+                    State::Idle
+                } else {
+                    State::Phasers(Count::new(30))
+                }
+            },
+
+            State::Phasers(count) => {
+                match count.current {
+                    x if x == count.start => {
+                        red_1.set_low();
+                        red_2.set_low();
+                        green.set_low();
+                    },
+
+                    x if x % 3 == 0 => {
+                        if red_1.is_set_low() {
+                            red_1.set_high();
+                            red_2.set_high();
+                        } else {
+                            red_1.set_low();
+                            red_2.set_low();
+                        }
+                    },
+
+                    _ => (),
+                }
+
+
+                if count.current == 0 {
+                    State::Loading(Count::new(120))
+                } else {
+                    State::Phasers(count.decrement())
+                }
+            },
+        };
+
+        arduino_hal::delay_ms(16);
     }
 }

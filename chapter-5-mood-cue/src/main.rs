486d2b681ef4af2a56eaa70afb88c627871116b9
Viljami Peltola <viljami.peltola@iki.fi>
2023-12-30 08:05:23 +0200

    Cleanup with 'cargo fmt'

--- chapter-5-mood-cue/src/main.rs
+++ chapter-5-mood-cue/src/main.rs
@@ -35,7 +35,11 @@ fn angle_to_servo_ticks(angle: u16) -> u16 {
 
 fn adjust_range(servo_value: u16) -> u16 {
     let servo_value = RANGE_MIN - SERVO_MIN + servo_value;
-    if servo_value > RANGE_MAX { RANGE_MAX } else { servo_value }
+    if servo_value > RANGE_MAX {
+        RANGE_MAX
+    } else {
+        servo_value
+    }
 }
 
 #[arduino_hal::entry]
@@ -43,19 +47,19 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
     let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
-
     let potentiometer_pin = pins.a0.into_analog_input(&mut adc);
-
     pins.d9.into_output(); // Servo pin
+
     // - TC1 runs off a 250kHz clock, with 5000 counts per overflow => 50 Hz signal.
     // - Each count increases the duty-cycle by 4us.
     // - Use OC1A which is connected to D9 of the Arduino Uno.
     // Based on: https://github.com/Rahix/avr-hal/blob/main/examples/arduino-uno/src/bin/uno-manual-servo.rs
-    let tc1 = dp.TC1;
-    tc1.icr1.write(|w| w.bits(SERVO_COUNTS));
-    tc1.tccr1a
+    dp.TC1.icr1.write(|w| w.bits(SERVO_COUNTS));
+    dp.TC1
+        .tccr1a
         .write(|w| w.wgm1().bits(0b10).com1a().match_clear());
-    tc1.tccr1b
+    dp.TC1
+        .tccr1b
         .write(|w| w.wgm1().bits(0b11).cs1().prescale_64());
 
     print::init(arduino_hal::default_serial!(
@@ -75,7 +79,7 @@ fn main() -> ! {
         );
 
         // Range of motion adjusted to the product interface
-        tc1.ocr1a.write(|w| w.bits(adjust_range(servo_value)));
+        dp.TC1.ocr1a.write(|w| w.bits(adjust_range(servo_value)));
         arduino_hal::delay_ms(500);
     }
 }
